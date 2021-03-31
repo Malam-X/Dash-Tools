4182facbf36ff46bcd62017916f2f57a87a29885
Bomb-SQU4D <81680578+Bomb-SQU4D@users.noreply.github.com>
2021-03-31 17:01:53 +0700

    0.0.4

---
+++ about.py
@@ -0,0 +1,17 @@
+"""
+  DASH TOOLS
+  Author: Author: DR4G0N5, JARED, Bomb SQU4D
+  Version: 0.0.4
+"""
+import var
+def main():
+    while True:
+        var.print_lines()
+        print(var.PyColors.bold,"\n\t   << ABOUT US >>\n",
+            var.PyColors.Fg.light_green,"\tAuthor: DR4G0N5, JARED, Bomb SQU4D\n\tVersion: 0.0.4\n\n",var.PyColors.reset)
+        input('Press any key to continue...')
+        break
+
+
+if __name__ == "__main__":
+    main()
